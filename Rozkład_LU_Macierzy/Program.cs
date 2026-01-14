3d9001e8e9bda126ab9d45401f87e5e1df076678
Przemek <Przemek@DESKTOP-NPFJ8V6>
2026-01-14 17:18:23 +0100

    xdf

--- Rozkład_LU_Macierzy/Program.cs
+++ Rozkład_LU_Macierzy/Program.cs
@@ -1,169 +1,122 @@
 ﻿using System;
-using System.Diagnostics;
+using System.Collections.Generic;
+using System.IO;
 
-namespace Rozkład_LU_Macierzy
+namespace RozkladLU_Graf
 {
+    // Struktura odpowiadająca kolumnom z Twojego slajdu
+    public struct RekordGrafu
+    {
+        public int Nr;
+        public int W1, W2, W3;
+        public string Im, Ia2, Ia1;
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("=== Rozkład LU macierzy metodą Gaussa ===\n");
+            Console.WriteLine("=== Rozkład LU z generowaniem tabeli grafu ===\n");
 
-            Stopwatch stop = new Stopwatch();
-            // Przykładowa macierz 3x3
+            // Macierz 4x4 (żeby tabela była dłuższa, jak w przykładzie)
             double[,] macierz = {
-                { 2, -1, 3 },
-                { 4, 2, -1 },
-                { -2, 3, 1 }
+                { 2, -1, 3, 1 },
+                { 4, 2, -1, 3 },
+                { -2, 3, 1, 5 },
+                { 1, 1, 1, 1 }
             };
 
-            Console.WriteLine("Macierz początkowa:");
+            Console.WriteLine("Macierz wejściowa:");
             WyswietlMacierz(macierz);
 
-            var (L, U, historiaU, operacje) = RozkladLU_Z_Sladem(macierz);
+            // Uruchomienie algorytmu
+            var (L, U, tabela) = RozkladLU_Z_Generatorem(macierz);
 
-            ZapiszDoPliku(historiaU, operacje, "lu_graf_dane.txt");
+            // Wyświetlenie wyników
+            Console.WriteLine("Macierz L:");
+            WyswietlMacierz(L);
+            Console.WriteLine("Macierz U:");
+            WyswietlMacierz(U);
 
-            if (L != null && U != null)
-            {
-                Console.WriteLine("\nMacierz L (dolnotrójkątna):");
-                WyswietlMacierz(L);
+            // Zapis do pliku tekstowego (tabela jak ze slajdu)
+            ZapiszTabeleDoPliku(tabela, "tabela_grafu.txt");
 
-                Console.WriteLine("\nMacierz U (górnotrójkątna):");
-                WyswietlMacierz(U);
-
-                Console.WriteLine("\nSprawdzenie: L × U =");
-                var wynik = PomnoźMacierze(L, U);
-                WyswietlMacierz(wynik);
-            }
+            Console.WriteLine("Sukces! Tabela grafu została zapisana do pliku 'tabela_grafu.txt'.");
+            Console.WriteLine("Naciśnij dowolny klawisz, aby zamknąć...");
+            Console.ReadKey();
         }
 
-        static (double[,] L, double[,] U, List<double[,]> historiaU, List<string> historiaOperacji) RozkladLU_Z_Sladem(double[,] A)
+        static (double[,] L, double[,] U, List<RekordGrafu> tabela) RozkladLU_Z_Generatorem(double[,] A)
         {
             int n = A.GetLength(0);
-
-            double[,] U = KopiujMacierz(A);
+            double[,] U = (double[,])A.Clone();
             double[,] L = new double[n, n];
+            for (int i = 0; i < n; i++) L[i, i] = 1.0;
 
-            for (int i = 0; i < n; i++)
-                L[i, i] = 1.0;
+            List<RekordGrafu> tabela = new List<RekordGrafu>();
+            int licznik = 0;
 
-            List<double[,]> historiaU = [];
-            List<string> historiaOperacji = [];
-
-            // stan początkowy
-            historiaU.Add(KopiujMacierz(U));
-            historiaOperacji.Add("U(0) = A");
-
-            for (int k = 0; k < n - 1; k++)
+            // Pętle i1, i2, i3 (mapowanie: k, i, j)
+            // Zakresy ustawione tak, aby odpowiadały indeksowaniu od 1 (jak na slajdzie)
+            for (int k = 1; k <= n - 1; k++) // i1
             {
-                for (int i = k + 1; i < n; i++)
+                for (int i = k + 1; i <= n; i++) // i2
                 {
-                    // Gniazdo G1 – obliczenie mnożnika
-                    double mnoznik = U[i, k] / U[k, k];
-                    L[i, k] = mnoznik;
+                    // Obliczenie mnożnika (Gniazdo G1)
+                    double mnoznik = U[i - 1, k - 1] / U[k - 1, k - 1];
+                    L[i - 1, k - 1] = mnoznik;
 
-                    historiaOperacji.Add(
-                        $"G1: L[{i},{k}] = U[{i},{k}] / U[{k},{k}]"
-                    );
-
-                    for (int j = k; j < n; j++)
+                    for (int j = k + 1; j <= n; j++) // i3
                     {
-                        // Gniazdo G2 – aktualizacja U
-                        U[i, j] -= mnoznik * U[k, j];
+                        // Operacja aktualizacji (Gniazdo G2)
+                        U[i - 1, j - 1] -= mnoznik * U[k - 1, j - 1];
 
-                        historiaOperacji.Add(
-                            $"G2: U[{i},{j}] = U[{i},{j}] - L[{i},{k}] * U[{k},{j}]"
-                        );
-                    }
-                }
-
-                // zapis U po kroku k
-                historiaU.Add(KopiujMacierz(U));
-            }
-
-            return (L, U, historiaU, historiaOperacji);
-        }
-
-
-        static void WyswietlMacierz(double[,] macierz)
-        {
-            int wiersze = macierz.GetLength(0);
-            int kolumny = macierz.GetLength(1);
-
-            for (int i = 0; i < wiersze; i++)
-            {
-                Console.Write("| ");
-                for (int j = 0; j < kolumny; j++)
-                {
-                    Console.Write($"{macierz[i, j],8:F3} ");
-                }
-                Console.WriteLine("|");
-            }
-            Console.WriteLine();
-        }
-
-        static double[,] PomnoźMacierze(double[,] A, double[,] B)
-        {
-            int n = A.GetLength(0);
-            double[,] wynik = new double[n, n];
-
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    wynik[i, j] = 0;
-                    for (int k = 0; k < n; k++)
-                    {
-                        wynik[i, j] += A[i, k] * B[k, j];
+                        // Rejestracja wiersza tabeli
+                        licznik++;
+                        tabela.Add(new RekordGrafu
+                        {
+                            Nr = licznik,
+                            W1 = k,
+                            W2 = i,
+                            W3 = j,
+                            Im = $"<{i},{k}>",   // m[i2, i1]
+                            Ia2 = $"<{k},{j}>",  // a[i1, i3]
+                            Ia1 = $"<{i},{j}>"   // a[i2, i3]
+                        });
                     }
                 }
             }
-
-            return wynik;
+            return (L, U, tabela);
         }
 
-        static double[,] KopiujMacierz(double[,] M)
-        {
-            int n = M.GetLength(0);
-            double[,] kopia = new double[n, n];
-
-            for (int i = 0; i < n; i++)
-                for (int j = 0; j < n; j++)
-                    kopia[i, j] = M[i, j];
-
-            return kopia;
-        }
-
-        static void ZapiszDoPliku(List<double[,]> historiaU,List<string> operacje,string nazwaPliku)
+        static void ZapiszTabeleDoPliku(List<RekordGrafu> tabela, string nazwaPliku)
         {
             using (StreamWriter sw = new StreamWriter(nazwaPliku))
             {
-                sw.WriteLine("=== HISTORIA ALGORYTMU LU ===\n");
+                sw.WriteLine("Konstruowanie grafów algorytmów - Tabela G2");
+                sw.WriteLine("------------------------------------------------------------------");
+                sw.WriteLine("{0,-4} | {1,-2} {2,-2} {3,-2} | {4,-8} | {5,-8} | {6,-8}",
+                             "nr", "W1", "W2", "W3", "Im[nr]", "Ia2[nr]", "Ia1[nr]");
+                sw.WriteLine("------------------------------------------------------------------");
 
-                for (int k = 0; k < historiaU.Count; k++)
+                foreach (var r in tabela)
                 {
-                    sw.WriteLine($"--- U po kroku {k} ---");
-                    ZapiszMacierz(sw, historiaU[k]);
-                    sw.WriteLine();
+                    sw.WriteLine("{0,-4} | {1,-2} {2,-2} {3,-2} | {4,-8} | {5,-8} | {6,-8}",
+                        r.Nr, r.W1, r.W2, r.W3, r.Im, r.Ia2, r.Ia1);
                 }
-
-                sw.WriteLine("=== OPERACJE (GNIAZDA GRAFU) ===\n");
-                foreach (var op in operacje)
-                    sw.WriteLine(op);
             }
         }
 
-        static void ZapiszMacierz(StreamWriter sw, double[,] M)
+        static void WyswietlMacierz(double[,] M)
         {
             int n = M.GetLength(0);
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < n; j++)
-                    sw.Write($"{M[i, j],8:F3} ");
-                sw.WriteLine();
+                    Console.Write($"{M[i, j],8:F2} ");
+                Console.WriteLine();
             }
+            Console.WriteLine();
         }
-
     }
 }
